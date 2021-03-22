4d64ab24d3439f080783e59cda107e9c4ce47dbf
hyeongcheolkim <tori1753@naver.com>
2021-03-22 11:09:53 +0900

    EX3_2

--- Java/uos_orientedprgm/assignment/ex3/EX3_2.java
+++ Java/uos_orientedprgm/assignment/ex3/EX3_2.java
@@ -1,15 +1,15 @@
 public class EX3_2 {
     public static void main(String[] args) throws Exception {
-        int n = 100 + 1;
-        boolean[] arr = new boolean[n];
-        for (int i = 2; i < n; i++)
+
+        boolean[] arr = new boolean[100+1];
+        for (int i = 2; i <= 100; i++)
             arr[i] = true;
 
-        for (int i = 2; i*i < n; i++)
-            for (int j = i + i; j <= n; j += i)
+        for (int i = 2; i*i < 100; i++)
+            for (int j = i + i; j <= 100; j += i)
                 arr[j] = false;
 
-        for (int i = 2; i < n; i++)
+        for (int i = 2; i <= 100; i++)
             if(arr[i])
                 System.out.print(i+" ");
     }
