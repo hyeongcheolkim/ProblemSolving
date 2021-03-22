72da0f72667a3d246db7eff346fed29019754aae
hyeongcheolkim <tori1753@naver.com>
2021-03-22 10:43:13 +0900

    EX3_3

--- Java/uos_orientedprgm/assignment/ex3/EX3_3.java
+++ Java/uos_orientedprgm/assignment/ex3/EX3_3.java
@@ -1,13 +1,10 @@
 
-
-public class EX3_3
-{
-    public static void main(String[] args)
-    {
-        for(int i=1;i<100;i++)
-            for(int j=1;j<100;j++)
-                for(int k=1;k<100;k++)
-                    if(i*i+j*j==k*k||j*j+k*k==i*i||i*i+k*k==j*j)
-                        System.out.println(i+" "+j+" "+k);
-    }   
+public class EX3_3 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i++)
+            for (int j = 1; j < 100; j++)
+                for (int k = 1; k < 100; k++)
+                    if (i * i + j * j == k * k || j * j + k * k == i * i || i * i + k * k == j * j)
+                        System.out.println(i + " " + j + " " + k);
+    }
 }
