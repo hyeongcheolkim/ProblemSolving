ceae7b09785ade2aab1a9366333b45d88a9ca362
hyeongcheolkim <tori1753@naver.com>
2021-03-22 18:23:36 +0900

    EX3_1

--- Java/uos_orientedprgm/assignment/ex3/EX3_1.java
+++ Java/uos_orientedprgm/assignment/ex3/EX3_1.java
@@ -4,15 +4,22 @@
 
 public class EX3_1 {
     public static void main(String[] args) throws Exception {
-        int n = 5;// 홀수만 가능.
-        for(int i=n;i>=1;i-=2)
-        {
-            for(int j=0;j<(n-i);j++)
+        final int n = 5; // n은 홀수.
+        int star;
+        boolean flag = true;
+
+        //blank*2+star = 5 항등식 이용
+        //n부터 시작하여 1까지 star += -2. 그후 flag를 false로 바꾸어 n까지 star += 2.
+        for (star = n; star <= n; star+=(flag?-2:2)){
+            for (int blank = 0; blank < (n - star) / 2; blank++)
                 System.out.print(" ");
-            for(int j=0;j<i;i++)
+            for (int i = 0; i < star; i++)
                 System.out.print("*");
             System.out.print('\n');
+            if(star==1)
+                flag=false;
         }
-        
+
+
     }
 }
