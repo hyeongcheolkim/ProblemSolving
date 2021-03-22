46fba1ce3371a25826da7ad70d57bfe82c1c502e
hyeongcheolkim <tori1753@naver.com>
2021-03-22 11:05:57 +0900

    EX3_4

--- Java/uos_orientedprgm/assignment/ex3/EX3_4.java
+++ Java/uos_orientedprgm/assignment/ex3/EX3_4.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class EX3_4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.print("연산을 입력하세요: ");
         char opt = sc.next().charAt(0);
         System.out.print("숫자 2개를 입력하세요: ");
@@ -27,6 +27,8 @@ public class EX3_4 {
             }
             System.out.printf("%.1f / %.1f = %.1f", a, b, a / b);
             break;
+        default:
+            System.out.printf("정의되지 않은 연산자입니다. (+)(-)(*)(/)연산만 가능합니다.)");
         }
 
         sc.close();
