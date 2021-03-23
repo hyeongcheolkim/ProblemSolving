15766b1f20be06ff8262a32d6fa4d89e900e9b1d
hyeongcheolkim <tori1753@naver.com>
2021-03-23 15:39:58 +0900

    cpp algorithm ps number 29

--- C++/ps_algorithmlec/29/AA.cpp
+++ C++/ps_algorithmlec/29/AA.cpp
@@ -6,6 +6,20 @@ using namespace std;
 
 int main()
 {
-    
+    int n;
+    cin >> n;
+    int num = 0;
+    for (int i = 1; i <= n;i++)
+    {
+        static int temp = 0;
+        temp = i;
+        while(temp!=0)
+        {
+            if(temp%10==3)
+                num++;
+            temp /= 10;
+        }
+    }
+    cout << num;
     return 0;
 }
