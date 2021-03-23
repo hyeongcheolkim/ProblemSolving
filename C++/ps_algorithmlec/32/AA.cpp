3a8ec86745a6b4fc8a1fccbf570069bdaec4c462
hyeongcheolkim <tori1753@naver.com>
2021-03-23 21:48:41 +0900

    cpp algorithm ps number 32

---
+++ C++/ps_algorithmlec/32/AA.cpp
@@ -0,0 +1,34 @@
+// cpp algorithm ps number 32
+
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    auto arr = new int[n];
+
+    for (int i = 0; i < n;i++)
+        cin >> arr[i];
+
+    for (int i = 0; i < n-1;i++)
+    {
+        static int min_index = -1;
+        static int min = 2700000;
+        min = 2700000;
+        for (int j = i+1; j < n; j++)
+            if(arr[j]<min)
+            {
+                min = arr[j];
+                min_index = j;
+            }
+        swap(arr[i], arr[min_index]);
+    }
+    for (int i = 0; i < n;i++)
+        cout << arr[i]<< " ";
+            return 0;
+}
