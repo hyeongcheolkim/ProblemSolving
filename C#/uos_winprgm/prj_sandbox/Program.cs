7e64490df8bfee537720c09413364f55c7662be4
hyeongcheolkim <tori1753@naver.com>
2021-03-24 21:46:09 +0900

    Read Write prc

--- C#/uos_winprgm/prj_sandbox/Program.cs
+++ C#/uos_winprgm/prj_sandbox/Program.cs
@@ -4,21 +4,15 @@ namespace prj_sandbox
 {
     class Program
     {
-        static void IsOperator(object obj)
-        {
-            Console.WriteLine(obj + "is int:" + (obj is int));
-            Console.WriteLine(obj + "is string:" + (obj is string));
-        }
-        static void AsOperator(object obj)
-        {
-            Console.WriteLine(obj + "as string ==null:" + (obj as string == null));
-        }
         static void Main(string[] args)
         {
-            IsOperator(10);
-            IsOperator("ABC");
-            AsOperator(10);
-            AsOperator("ABC");
+            int i;
+            char c;
+            Console.Write("Enter a digit a character=");
+            i = Console.Read() - '0';
+            c = (char)Console.Read();
+            Console.Write("i= " + i);
+            Console.WriteLine(",c=" + c);
         }
     }
 }
