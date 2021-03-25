47bb0620cb5a39c3320fd29615d9f192e593562f
hyeongcheolkim <tori1753@naver.com>
2021-03-25 15:55:18 +0900

    cpp algorithm ps number 39

---
+++ C++/ps_algorithmlec/39/AA.cpp
@@ -0,0 +1,69 @@
+// cpp algorithm ps number 39
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    bool arr1End = true, arr2End = true;
+
+    cin >> n;
+    auto arr1 = new int[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr1[i];
+
+    cin >> m;
+    auto arr2 = new int[m];
+    for (int i = 0; i < m; i++)
+        cin >> arr2[i];
+
+    auto res = new int[n + m]{0};
+
+    while (arr1End || arr2End)
+    {
+        static int cur1 = 0, cur2 = 0, curRes = 0;
+        while (arr1End && arr2End)
+        {
+            if (arr1[cur1] <= arr2[cur2])
+            {
+                res[curRes] = move(arr1[cur1]);
+                cur1++;
+                curRes++;
+            }
+            if (arr1[cur1] > arr2[cur2])
+            {
+                res[curRes] = move(arr2[cur2]);
+                cur2++;
+                curRes++;
+            }
+            if (cur1 == n)
+                arr1End = false;
+            if (cur2 == m)
+                arr2End = false;
+        }
+        if (arr1End)
+        {
+            res[curRes] = move(arr1[cur1]);
+            cur1++;
+            curRes++;
+        }
+        if (arr2End)
+        {
+            res[curRes] = move(arr2[cur2]);
+            cur2++;
+            curRes++;
+        }
+
+        if (cur1 == n)
+            arr1End = false;
+        if (cur2 == m)
+            arr2End = false;
+    }
+
+    for (int i = 0; i < (n + m); i++)
+        cout << res[i];
+
+    return 0;
+}
