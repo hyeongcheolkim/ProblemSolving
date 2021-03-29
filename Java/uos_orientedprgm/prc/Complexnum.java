fb4333e708a268ef19bcde4d0da8b60bf01280c9
hyeongcheolkim <tori1753@naver.com>
2021-03-29 11:40:36 +0900

    Complexnum

---
+++ Java/uos_orientedprgm/prc/Complexnum.java
@@ -0,0 +1,32 @@
+public class Complexnum {
+    public static void main(String[] args) throws Exception {
+        Complex complex = new Complex();
+        complex.print();
+        System.out.print('\n');
+        complex.setReal(10).setImage(20).print();
+    }
+}
+
+class Complex {
+    private int real;
+    private int image;
+
+    Complex() {
+        real = 0;
+        image = 0;
+    }
+
+    Complex setReal(int real) {
+        this.real = real;
+        return this;
+    }
+
+    Complex setImage(int image) {
+        this.image = image;
+        return this;
+    }
+
+    void print() {
+        System.out.printf("%d+%di", real, image);
+    }
+}
