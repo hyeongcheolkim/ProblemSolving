f1c387e738af6986083421d022367211d74f0978
hyeongcheolkim <tori1753@naver.com>
2021-03-29 10:32:31 +0900

    StringTest

--- Java/uos_orientedprgm/prc/StringTest.java
+++ Java/uos_orientedprgm/prc/StringTest.java
@@ -1,9 +1,7 @@
 public class StringTest {
     public static void main(String[] args) throws Exception {
         String s1 = new String("Java");
-        String s2 = new String("Java");
         String s3 = "Java";
-        String s4 = "Java";
 
         // System.out.println(s1==s2);
         // System.out.println(s3==s4);
