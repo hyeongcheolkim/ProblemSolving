1f227dcdf834fd5187c3884be83d3a5f746f4694
hyeongcheolkim <tori1753@naver.com>
2021-03-31 23:23:13 +0900

    call by val, ptr, ref.

--- C#/uos_winprgm/prj_sandbox/Program.cs
+++ C#/uos_winprgm/prj_sandbox/Program.cs
@@ -4,9 +4,23 @@ namespace prj_sandbox
 {
     class Program
     {
+        int i;
+        Program(int i)
+        {
+            this.i = i;
+        }
+        static void Swap( Program x,  Program y)
+        {
+            Program temp = x;
+            x = y;
+            y = temp;
+        }
         static void Main(string[] args)
         {
-            Console.Write(-4 % 3);
+            Program a = new Program(1);
+            Program b = new Program(2);
+            Swap( a,  b);
+            Console.Write(a.i+"hi"+ b.i);
         }
     }
 }
