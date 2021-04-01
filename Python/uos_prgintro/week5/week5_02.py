257d657ef2742fe939b63dd45177f8f9449f4895
hyeongcheolkim <tori1753@naver.com>
2021-04-01 21:31:53 +0900

    python week5 assignment

---
+++ Python/uos_prgintro/week5/week5_02.py
@@ -0,0 +1,9 @@
+# University of Seoul. Dept of Computer Science.
+# 2018430021 김형철
+# week5_02
+
+temperature = int(input("현재 온도를 입력하세요 :"))
+if temperature>=35:
+    print("매우 덥습니다")
+elif temperature>=30:
+    print("덥습니다")
