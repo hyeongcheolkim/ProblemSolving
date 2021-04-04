32e510b2e89a4f74efc03f8677f9c7edec5e266e
hyeongcheolkim <tori1753@naver.com>
2021-04-04 20:39:26 +0900

    stack

--- C++/prc_sandbox/sandbox.cpp
+++ C++/prc_sandbox/sandbox.cpp
@@ -1,25 +1,11 @@
 #include <iostream>
-#include <vector>
+#include <stack>
 
 using namespace std;
 
-void fnc(vector<vector<int>> &arr, const int &n, const int &m);
-
 int main()
 {
-    int n = 5, m = 5;
-    vector<vector<int>> arr(n,vector<int>(m));
-    
-    fnc(arr, n, m);
-
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            cout << arr[i][j] << "# ";
+    stack<int> arr;
+    cout << int(arr.top());
 }
 
-void fnc( vector<vector<int>> &arr, const int &n, const int &m)
-{
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            arr[i][j] = i*10+j ;
-}
