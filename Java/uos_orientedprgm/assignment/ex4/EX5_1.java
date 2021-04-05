4aafa4eeafc8b5118ea7b4894c71953492b5549f
hyeongcheolkim <tori1753@naver.com>
2021-04-05 15:56:27 +0900

    EX5_1

---
+++ Java/uos_orientedprgm/assignment/ex4/EX5_1.java
@@ -0,0 +1,44 @@
+// University of Seoul. Dept of Computer Science.
+// 2018430021 김형철
+// EX5_1
+
+import java.util.Scanner;
+
+public class EX5_1 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        final int SIZE = 10;
+        int[] seat = new int[SIZE];
+        int cnt = 0;
+        while (true) {
+            System.out.println("====================");
+            for (int i = 0; i < SIZE; i++) {
+                System.out.print(i + " ");
+            }
+            System.out.println("\n====================");
+            for (int i = 0; i < SIZE; i++) {
+                System.out.print(seat[i] + " ");
+            }
+            System.out.println("\n====================");
+            if (cnt == 10) {
+                System.out.println(">좌석이 꽉찼습니다. 예약이 불가능합니다<");
+                break;
+            }
+            int num;
+            System.out.print("몇번째 좌석을 예약하시겠습니까? : ");
+            num = sc.nextInt();
+            if (!(0 <= num && num <= 9)) {
+                System.out.println(">1과 10사이의 좌석을 골라주십시오<");
+                continue;
+            }
+            if (seat[num] == 1) {
+                System.out.println(">이미 예약된 자리입니다. 다른자리를 골라주세요<");
+                continue;
+            }
+            seat[num] = 1;
+            System.out.println(">"+num + "자리로 예약 완료되었습니다<");
+            cnt++;
+        }
+        sc.close();
+    }
+}
