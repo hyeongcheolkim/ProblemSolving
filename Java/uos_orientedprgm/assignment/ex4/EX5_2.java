e452e1b2362f27bbf3842552caeff5d82a10a750
hyeongcheolkim <tori1753@naver.com>
2021-04-05 17:58:21 +0900

    EX5_2

--- Java/uos_orientedprgm/assignment/ex4/EX5_2.java
+++ Java/uos_orientedprgm/assignment/ex4/EX5_2.java
@@ -4,37 +4,34 @@
 
 public class EX5_2 {
     static public void main(String args[]) {
-        PlaneTest test = new PlaneTest();
-        for (int i = 0; i < test.size(); i++) {
-            System.out.print("제작사:"+test.getplane(i).getmanufacturer()+" ");
-            System.out.print("모델:"+test.getplane(i).getmodel()+" ");
-            System.out.print("최대승객:"+test.getplane(i).getpassengers()+" \n");
-        }
-        System.out.print("항공기 대수 = " + Plane.getPlanes()+"대");
+        PlaneTest test = new PlaneTest(3);
+        test.print();
+        System.out.print("항공기 대수 = " + Plane.getPlanes() + "대");
     }
 }
 
 class PlaneTest {
-    private final int SIZE = 3;
-    private Plane[] planearr = new Plane[SIZE];
+    private int SIZE; // SIZE=비행기 갯수
+    private Plane[] planearr;
 
-    public PlaneTest() {
+    public PlaneTest(int SIZE) {
+        this.SIZE = SIZE;
+        planearr = new Plane[SIZE];
         for (int i = 0; i < SIZE; i++)
             planearr[i] = new Plane();
-        //만약 비행기를 더 추가하고 싶을경우 SIZE를 수정후, 데이터를 여기에 추가
+        // 3대의 정보를 미리 받았으므로, SIZE>=3이어야함. 4대째부터는 default 정보를 갖는다
         planearr[0].setmanufacturer("에어버스").setmodel("A380").setpassengers(500);
         planearr[1].setmanufacturer("보잉").setmodel("B777").setpassengers(400);
         planearr[2].setmanufacturer("보잉").setmodel("B747").setpassengers(500);
     }
-
-    public Plane getplane(int index) {
-        return this.planearr[index];
+    //PlaneTest에서 만든 비행기 리스트 출력
+    public void print() {
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print("제작사:" + planearr[i].getmanufacturer() + " ");
+            System.out.print("모델:" + planearr[i].getmodel() + " ");
+            System.out.print("최대승객:" + planearr[i].getpassengers() + " \n");
+        }
     }
-
-    public int size() {
-        return SIZE;
-    }
-
 }
 
 class Plane {
