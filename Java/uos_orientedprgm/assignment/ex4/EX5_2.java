defaae5e9f74f88309dc13eb7bfde3162bc89ab7
hyeongcheolkim <tori1753@naver.com>
2021-04-05 17:00:24 +0900

    EX5_2

--- Java/uos_orientedprgm/assignment/ex4/EX5_2.java
+++ Java/uos_orientedprgm/assignment/ex4/EX5_2.java
@@ -6,12 +6,11 @@ public class EX5_2 {
     static public void main(String args[]) {
         PlaneTest test = new PlaneTest();
         for (int i = 0; i < test.size(); i++) {
-            System.out.print(test.getplane(i).getmanufacturer()+" ");
-            System.out.print(test.getplane(i).getmodel()+" ");
-            System.out.print(test.getplane(i).getpassengers()+" \n");
+            System.out.print("제작사:"+test.getplane(i).getmanufacturer()+" ");
+            System.out.print("모델:"+test.getplane(i).getmodel()+" ");
+            System.out.print("최대승객:"+test.getplane(i).getpassengers()+" \n");
         }
-
-        System.out.print("항공기 대수 = " + Plane.getPlanes());
+        System.out.print("항공기 대수 = " + Plane.getPlanes()+"대");
     }
 }
 
@@ -22,6 +21,7 @@ class PlaneTest {
     public PlaneTest() {
         for (int i = 0; i < SIZE; i++)
             planearr[i] = new Plane();
+        //만약 비행기를 더 추가하고 싶을경우 SIZE를 수정후, 데이터를 여기에 추가
         planearr[0].setmanufacturer("에어버스").setmodel("A380").setpassengers(500);
         planearr[1].setmanufacturer("보잉").setmodel("B777").setpassengers(400);
         planearr[2].setmanufacturer("보잉").setmodel("B747").setpassengers(500);
