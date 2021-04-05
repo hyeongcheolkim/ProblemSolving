f9de97eb202b030585509c1184a42d3c2bf4527d
hyeongcheolkim <tori1753@naver.com>
2021-04-05 18:06:36 +0900

    EX5_1

--- Java/uos_orientedprgm/assignment/ex4/EX5_1.java
+++ Java/uos_orientedprgm/assignment/ex4/EX5_1.java
@@ -10,16 +10,20 @@ public class EX5_1 {
         final int SIZE = 10;
         int[] seat = new int[SIZE];
         int cnt = 0;
+        System.out.println("===============================");
+        System.out.println("-1을 입력하면 예약을 종료합니다");
         while (true) {
-            System.out.println("====================");
+            System.out.println("===============================");
+            System.out.print("좌석번호| ");
             for (int i = 0; i < SIZE; i++) {
                 System.out.print(i + " ");
             }
-            System.out.println("\n====================");
+            System.out.println("\n===============================");
+            System.out.print("예약여부| ");
             for (int i = 0; i < SIZE; i++) {
                 System.out.print(seat[i] + " ");
             }
-            System.out.println("\n====================");
+            System.out.println("\n===============================");
             if (cnt == 10) {
                 System.out.println(">좌석이 꽉찼습니다. 예약이 불가능합니다<");
                 break;
@@ -27,6 +31,11 @@ public class EX5_1 {
             int num;
             System.out.print("몇번째 좌석을 예약하시겠습니까? : ");
             num = sc.nextInt();
+            if (num == -1)
+            {
+                System.out.print("예약을 종료합니다.");
+                break;
+            }
             if (!(0 <= num && num <= 9)) {
                 System.out.println(">1과 10사이의 좌석을 골라주십시오<");
                 continue;
