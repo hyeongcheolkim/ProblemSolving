7e5426512dbbd1cee8eb3477694def85d099cf03
hyeongcheolkim <tori1753@naver.com>
2021-04-08 15:01:20 +0900

    cpp algorithm ps number 72

---
+++ C++/ps_algorithmlec/72/AA.cpp
@@ -0,0 +1,27 @@
+// cpp algorithm ps number 72
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    queue<int> arr;
+    for (int i = 1; i <= n; i++)
+        arr.push(i);
+    for (int i = 1; arr.size() != 1; i++)
+        if (i % 3 != 0)
+        {
+            static int temp;
+            temp = arr.front();
+            arr.pop();
+            arr.push(temp);
+        }
+        else
+            arr.pop();
+    cout << arr.front();
+    return 0;
+}
