9e7c94cdf32d39431c31531939c530439e567038
hyeongcheolkim <tori1753@naver.com>
2021-04-10 14:24:08 +0900

    cpp algorithm ps number 76. using memoization

--- C++/ps_algorithmlec/76/AA.cpp
+++ C++/ps_algorithmlec/76/AA.cpp
@@ -1,30 +1,27 @@
 // cpp algorithm ps number 76
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int fnc(int n, const int&r)
+int dfs(int n, int r, vector<vector<int>>&memo)
 {
-    static int index = 0;
-    index++;
-    if(r==index)
-        return n;
-    return n * fnc(n - 1,r);
-}
-
-int factorial(int n)
-{
-    if(n==1||n==0)
+    if(memo[n][r]!=0)
+        return memo[n][r];
+    if(n==r || r==0)
+    {
+        memo[n][r] = 1;
         return 1;
-    return n * factorial(n - 1);
+    }
+    return memo[n][r]=dfs(n - 1, r - 1, memo) + dfs(n - 1, r, memo);
 }
 
 int main()
 {
     int n, r;
     cin >> n >> r;
-    r = (r <= n - r) ? (r) : (n - r);
-    cout << (fnc(n,r)/factorial(r));
+    vector<vector<int>> memo(n+1,vector<int>(n+1,0));
+    cout << dfs(n, r, memo);
     return 0;
 }
