257faa6618a3d5c581c818ba1b2c250d2b040f1a
hyeongcheolkim <tori1753@naver.com>
2021-04-13 23:42:20 +0900

    cpp algorithm ps number 82

---
+++ C++/ps_algorithmlec/82/AA.cpp
@@ -0,0 +1,46 @@
+// cpp algorithm ps number 82
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, r;
+
+void dfs(int level, vector<int> &arr)
+{
+    static int cnt = 0;
+    static vector<int> temp;
+    static vector<bool> trace(n, true);
+
+    if (temp.size() == r)
+    {
+        for (const auto &e : temp)
+            cout << e << ' ';
+        cout << '\n';
+
+            return;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (trace[i])
+        {
+            temp.push_back(arr[i]);
+            trace[i] = false;
+            dfs(level + 1, arr);
+            temp.pop_back();
+            trace[i] = true;
+        }
+    }
+}
+
+int main()
+{
+    cin >> n >> r;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    dfs(0, arr);
+    return 0;
+}
