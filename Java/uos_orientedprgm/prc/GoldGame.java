0d2366f1ee3a65f4e75f248b45b6ad5956057d87
hyeongcheolkim <tori1753@naver.com>
2021-04-14 22:15:29 +0900

    GoldGame

--- Java/uos_orientedprgm/prc/GoldGame.java
+++ Java/uos_orientedprgm/prc/GoldGame.java
@@ -12,12 +12,12 @@ public class GoldGame {
             Map.print();
             System.out.println("h:좌 j:상 k:하 l:우");
             char input = sc.nextLine().charAt(0);
-            //몬스터의 move를 후순위로 배치함으로서, 사망판정시 M가 @를 가리게 한다.
+            // 몬스터의 move를 후순위로 배치함으로서, 사망판정시 M가 @를 가리게 한다.
             main.move(input);
             monster.move('\0'); // 몬스터는 자동으로 움직이므로 함수인수 없음.
 
-            //Rule:동시에 몬스터와 주인공이 골드를 가질경우, 주인공이 몬스터에 죽는걸로 판정
-            if (main.compare(monster)) { 
+            // Rule:동시에 몬스터와 주인공이 골드를 가질경우, 주인공이 몬스터에 죽는걸로 판정
+            if (main.compare(monster)) {
                 System.out.println("몬스터를 만나 사망하였습니다...");
                 Map.print();
                 break;
@@ -38,8 +38,8 @@ public class GoldGame {
 }
 
 class Map {
-    static final int rowsize = 10; //게임판의 row사이즈
-    static final int columnsize = 19; //게임판의 column사이즈
+    static final int rowsize = 10; // 게임판의 row사이즈
+    static final int columnsize = 19; // 게임판의 column사이즈
     static private char[][] map = new char[rowsize][columnsize];
     static {
         for (int i = 0; i < rowsize; i++)
@@ -93,29 +93,38 @@ class Main extends Sprite {
     }
 
     void move(char c) {
-        if (Map.get(row, column) == '@') //주인공이 있던곳에 다른 물체가 오지 않았으면
-            Map.check(row, column, ' '); //주인공이 있던던곳을 지운다
+        if (Map.get(row, column) == '@') // 주인공이 있던곳에 다른 물체가 오지 않았으면
+            Map.check(row, column, ' '); // 주인공이 있던던곳을 지운다
         boolean outofmap = false;
-        if (c == 'h')
+        switch (c) {
+        case 'h':
             if (--column == 0) {
                 column++;
                 outofmap = true;
             }
-        if (c == 'j')
+            break;
+        case 'j':
             if (--row == 0) {
                 row++;
                 outofmap = true;
             }
-        if (c == 'k')
+            break;
+        case 'k':
             if (++row == Map.rowsize - 1) {
                 row--;
                 outofmap = true;
             }
-        if (c == 'l')
+            break;
+        case 'l':
             if (++column == Map.columnsize - 1) {
                 column--;
                 outofmap = true;
             }
+            break;
+        default:
+            System.out.println("유효하지 않은 입력입니다.");
+            break;
+        }
         if (outofmap)
             System.out.println("벽에 부딪혔습니다!!");
         Map.check(row, column, '@');
@@ -142,8 +151,8 @@ class Monster extends Sprite {
 
     void move(char c) {
         Random rand = new Random();
-        if (Map.get(row, column) == 'M') //몬스터가 있던곳에 다른 물체가 오지 않았다면
-            Map.check(row, column, ' '); //몬스터가 있던곳을 지운다
+        if (Map.get(row, column) == 'M') // 몬스터가 있던곳에 다른 물체가 오지 않았다면
+            Map.check(row, column, ' '); // 몬스터가 있던곳을 지운다
         if (rand.nextBoolean()) {
             row += (rand.nextBoolean()) ? 1 : -1;
             if (row == 0)
