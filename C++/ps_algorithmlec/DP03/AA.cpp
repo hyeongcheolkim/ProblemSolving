8e3338dc426091b6a8a4c33e4d5542322eea6811
hyeongcheolkim <tori1753@naver.com>
2021-04-17 18:26:56 +0900

    cpp algorithm ps number DP03

---
+++ C++/ps_algorithmlec/DP03/AA.cpp
@@ -0,0 +1,26 @@
+// cpp algorithm ps number DP03
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+int memo[50];
+
+int dfs(int n)
+{
+    if(memo[n]!=0)
+        return memo[n];
+    return memo[n] = dfs(n - 1) + dfs(n - 2);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin >> n;
+    memo[1] = 1;
+    memo[2] = 2;
+    cout << dfs(n);
+    return 0;
+}
