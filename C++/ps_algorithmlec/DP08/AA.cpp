0a08e1e6c3cc9ef2ca7b8e2de79ca914b72fcbd5
hyeongcheolkim <tori1753@naver.com>
2021-04-18 13:12:48 +0900

    cpp algorithm ps number DP08

---
+++ C++/ps_algorithmlec/DP08/AA.cpp
@@ -0,0 +1,38 @@
+// cpp algorithm ps number DP08
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int recursion(const int &row, const int& column, int sum, const vector<vector<int>> &map,vector<vector<int>> &dp )
+{
+    if(dp[row][column]==100)
+        dp[row][column] = sum;
+    else if(dp[row][column]<sum)
+        return 10000000;
+    else if(dp[row][column]>sum)
+        dp[row][column] = sum;
+
+    if(map[row][column]==100)
+        return sum;
+    if(row==1 && column==1)
+        return sum;
+    return min(recursion(row - 1, column,sum+map[row-1][column], map,dp), recursion(row, column - 1,sum+map[row][column-1], map,dp));
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    vector<vector<int>> map(n + 2, vector<int>(n + 2, 100));
+    vector<vector<int>> dp(n + 2, vector<int>(n + 2, 100));
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+        {
+            cin >> map[i][j];
+        }
+    cout << recursion(n, n, map[n][n], map, dp);
+    return 0;
+}
