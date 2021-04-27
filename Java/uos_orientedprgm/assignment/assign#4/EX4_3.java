ecccc5b213f208432f94740465da3b6538a26386
hyeongcheolkim <tori1753@naver.com>
2021-04-27 15:50:40 +0900

    EX4_3

--- Java/uos_orientedprgm/assignment/assign#4/EX4_3.java
+++ Java/uos_orientedprgm/assignment/assign#4/EX4_3.java
@@ -3,10 +3,12 @@ import java.util.*;
 public class EX4_3 {
     public static void main(String[] args) {
         int cnt = 0;
+        boolean[] check;
         Scanner sc = new Scanner(System.in);
         StringBuilder problem = new StringBuilder();
         System.out.print("출제할 단어를 적으세요:");
         String word = sc.nextLine();
+        check = new boolean[word.length()];
         for (int i = 0; i < word.length(); i++)
             problem.append('_');
         System.out.println(word.length() + "글자 입니다");
@@ -16,10 +18,11 @@ public class EX4_3 {
             char input;
             input = sc.nextLine().charAt(0);
             for (int i = 0; i < word.length(); i++)
-                if (word.charAt(i) == input) {
+                if (word.charAt(i) == input && !check[i]) {
                     System.out.println("맞췄습니다!");
                     problem.setCharAt(i, input);
                     cnt++;
+                    check[i] = true;
                     flag = true;
                 }
             if (!flag)
