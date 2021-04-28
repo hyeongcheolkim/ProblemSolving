ff7c0a0a378ea38ed4b8aced9565855e6a408859
hyeongcheolkim <tori1753@naver.com>
2021-04-28 23:24:28 +0900

    baekjoon ps 2164

---
+++ C++/ps_baekjoon/2164/main.cpp
@@ -0,0 +1,24 @@
+//baekjoon ps 2164
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    queue<int> q;
+    for (int i = 1; i <= n; i++)
+        q.push(i);
+    while(q.size()!=1)
+    {
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+    cout << q.front();
+    return 0;
+}
