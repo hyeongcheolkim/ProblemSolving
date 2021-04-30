b837cebb2496a9051567ab6c6c7bf432c04f6fd5
hyeongcheolkim <tori1753@naver.com>
2021-04-30 22:07:05 +0900

    prc

--- JS/prc_deepdive/prc.js
+++ JS/prc_deepdive/prc.js
@@ -1,2 +1,23 @@
-if ('cat' && true)
-    console.log('hi')
+const person = {
+    firstname: "Ungmo",
+    lastname: "Lee",
+
+    get fullName() {
+        return `${this.firstname} ${this.lastname}`
+    },
+    set fullName(name) {
+        [this.firstname, this.lastname] = name.split(' ')
+    }
+};
+
+console.log(person.firstname + ' ' + person.lastname)
+person.fullName = 'Heegun Lee'
+console.log(person)
+
+console.log(person.fullName)
+
+let descriptor = Object.getOwnPropertyDescriptor(person, 'firstName')
+console.log(descriptor)
+
+descriptor = Object.getOwnPropertyDescriptor(person, "fullName")
+console.log(descriptor)
