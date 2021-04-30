e9bbbb1a6c8d1eb650113f8aa563e3eecdec9a3e
hyeongcheolkim <tori1753@naver.com>
2021-04-30 17:48:00 +0900

    baekjoon ps 1037

---
+++ C++/ps_baekjoon/1037/main.cpp
@@ -0,0 +1,20 @@
+//baekjoon ps 1037
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int n;
+    int res = 1;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    sort(arr.begin(), arr.end());
+    cout << arr.front() * arr.back();
+}
