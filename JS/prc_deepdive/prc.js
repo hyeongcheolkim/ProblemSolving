f45082c096b635e18789719e70a8fa97b5d2e355
hyeongcheolkim <tori1753@naver.com>
2021-04-30 23:59:16 +0900

    prc

--- JS/prc_deepdive/prc.js
+++ JS/prc_deepdive/prc.js
@@ -1,23 +1,12 @@
-const person = {
-    firstname: "Ungmo",
-    lastname: "Lee",
-
-    get fullName() {
-        return `${this.firstname} ${this.lastname}`
-    },
-    set fullName(name) {
-        [this.firstname, this.lastname] = name.split(' ')
+function Circle(radius) {
+    if (!new.target) {
+        return new Circle(radius)
     }
-};
+    this.radius = radius
+    this.getDiameter = function () {
+        return 2 * this.radius;
+    }
+}
 
-console.log(person.firstname + ' ' + person.lastname)
-person.fullName = 'Heegun Lee'
-console.log(person)
-
-console.log(person.fullName)
-
-let descriptor = Object.getOwnPropertyDescriptor(person, 'firstName')
-console.log(descriptor)
-
-descriptor = Object.getOwnPropertyDescriptor(person, "fullName")
-console.log(descriptor)
+const circle = Circle(5);
+console.log(circle.getDiameter());
