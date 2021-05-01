842e07981ad54fa84cfc3961837467f6d62066b7
hyeongcheolkim <tori1753@naver.com>
2021-05-01 15:47:48 +0900

    baekjoon ps 11050

--- C++/ps_baekjoon/11050/main.cpp
+++ C++/ps_baekjoon/11050/main.cpp
@@ -4,19 +4,21 @@
 
 using namespace std;
 
-int factorial(int n)
+int n, k;
+int memo[1001][1001];
+int bfs(int n, int k)
 {
-    int res = 1;
-    for (int i = 2; i <= n;i++)
-        res *= i;
-    return res;
+    if (memo[n][k] != 0)
+        return memo[n][k];
+    if (n == k || k == 0)
+        return 1;
+    return memo[n][k] = (bfs(n - 1, k) % 10007 + bfs(n - 1, k - 1) % 10007) % 10007;
 }
 
 int main()
 {
     ios_base::sync_with_stdio(false);
-    int n, k;
     cin >> n >> k;
-    cout << factorial(n) / factorial(k) / factorial(n - k);
+    cout << bfs(n, k);
     return 0;
 }
