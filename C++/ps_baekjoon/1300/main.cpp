cfb0f1bd287199f931f7abde90b30c562671d02f
hyeongcheolkim <tori1753@naver.com>
2021-05-04 12:39:07 +0900

    baekjoon ps 1300

---
+++ C++/ps_baekjoon/1300/main.cpp
@@ -0,0 +1,34 @@
+//baekjoon ps 1300
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int n, k;
+    cin >> n >> k;
+    ll lt, rt, mid, res;
+    lt = 1;
+    rt = k;
+    while (lt <= rt)
+    {
+        int mid = (lt + rt) / 2;
+        ll num = 0;
+        for (int i = 1; i <= n; i++)
+            num += min(mid / i, n);
+        if (num < k)
+            lt = mid + 1;
+        else
+        {
+            res = mid;
+            rt = mid - 1;
+        }
+    }
+    cout << res;
+    return 0;
+}
