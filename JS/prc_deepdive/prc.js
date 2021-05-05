02407259f44b3514de847d5f01f2aa5dff439d16
hyeongcheolkim <tori1753@naver.com>
2021-05-05 17:17:27 +0900

    js closer

--- JS/prc_deepdive/prc.js
+++ JS/prc_deepdive/prc.js
@@ -1,12 +1,21 @@
-function Circle(radius) {
-    if (!new.target) {
-        return new Circle(radius)
+const counter = (function () {
+    let num = 0;
+    return {
+        increse() {
+            return ++num;
+        },
+        decrease() {
+            return --num;
+        }
     }
-    this.radius = radius
-    this.getDiameter = function () {
-        return 2 * this.radius;
-    }
-}
+}());
 
-const circle = Circle(5);
-console.log(circle.getDiameter());
+
+console.log(counter.increse());
+console.log(counter.increse());
+console.log(counter.increse());
+console.log(counter.decrease());
+console.log(counter.decrease());
+console.log(counter.decrease());
+console.log(counter.decrease());
+console.log(counter.decrease());
