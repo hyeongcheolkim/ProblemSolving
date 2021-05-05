5ab25c02a6d77306f174ec260c398f1f493c5960
hyeongcheolkim <tori1753@naver.com>
2021-05-05 21:14:29 +0900

    prc

--- JS/prc_deepdive/prc.js
+++ JS/prc_deepdive/prc.js
@@ -1,21 +1,36 @@
-const counter = (function () {
-    let num = 0;
-    return {
-        increse() {
-            return ++num;
-        },
-        decrease() {
-            return --num;
-        }
+class Person {
+    constructor(name) {
+        this.name = name;
     }
-}());
 
+    sayHi() {
+        console.log("HI" + `${this.name}`);
+    }
 
-console.log(counter.increse());
-console.log(counter.increse());
-console.log(counter.increse());
-console.log(counter.decrease());
-console.log(counter.decrease());
-console.log(counter.decrease());
-console.log(counter.decrease());
-console.log(counter.decrease());
+    static sayHello() {
+        console.log("HELLO");
+    }
+}
+
+const Saram = (function () {
+    function Saram(name) {
+        this.name = name;
+    }
+
+    Saram.prototype.sayHi = function () {
+        console.log("HI" + `${this.name}`);
+    }
+
+    Saram.sayHello = function() {
+        console.log("HELLO");
+    }
+    return Saram;
+}())
+
+const me = new Person("KIM");
+me.sayHi();
+me.sayHello();
+
+const me2 = new Saram("LEE");
+Saram.sayHello();
+me2.sayHi();
