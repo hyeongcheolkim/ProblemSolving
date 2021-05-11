ceff130d09727e6035ecc25907d3d39800cee56b
hyeongcheolkim <tori1753@naver.com>
2021-05-12 00:40:39 +0900

    baekjoon ps 1956

---
+++ C++/ps_baekjoon/1956/main.cpp
@@ -0,0 +1,39 @@
+//baekjoon ps 1956
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    constexpr int INF = 200000000;
+    int v, e;
+    int res = INF;
+    cin >> v >> e;
+    vector<vector<int>> map(v + 1, vector<int>(v + 1, INF));
+    for (int i = 1; i <= v; i++)
+        map[i][i] = 0;
+    while (e--)
+    {
+        static int to, go, cost;
+        cin >> to >> go >> cost;
+        map[to][go] = cost;
+    }
+    for (int k = 1; k <= v; k++)
+        for (int i = 1; i <= v; i++)
+            for (int j = 1; j <= v; j++)
+                if (map[i][j] != INF)
+                    map[i][j] = min(map[i][j], map[i][k] + map[k][j]);
+    for (int i = 1; i <= v; i++)
+        for (int j = 1; j <= v; j++)
+            if (i != j)
+                res = min(res, map[i][j] + map[j][i]);
+    if(res==INF)
+        cout << -1;
+    else
+        cout << res;
+    return 0;
+}
