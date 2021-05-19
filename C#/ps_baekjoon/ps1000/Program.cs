5fd41ea318366af2d6fef19494e9bd69fd5b5fdb
hyeongcheolkim <tori1753@naver.com>
2021-05-20 00:48:48 +0900

    ps1000

---
+++ C#/ps_baekjoon/ps1000/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ps1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] input;
+            input = Console.ReadLine().Split(" ");
+            int a = Int32.Parse(input[0]);
+            int b = Int32.Parse(input[1]);
+            Console.Write(a + b);
+        }
+    }
+}
