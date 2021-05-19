b279961c7e8c13dc6ee80aba8f999f74085a1772
hyeongcheolkim <tori1753@naver.com>
2021-05-20 00:54:28 +0900

    set

--- Python/ps_baekjoon/1000/main.py
+++ Python/ps_baekjoon/1000/main.py
@@ -1,2 +1,5 @@
-#baekjoon ps xx
-
+#baekjoon ps 1000
+a,b = input().split(" ")
+a=int(a)
+b=int(b)
+print(a+b)
--- Python/ps_baekjoon/_default/main.py
+++ Python/ps_baekjoon/_default/main.py
@@ -1,5 +1,1 @@
-#baekjoon ps 1000
-a,b = input().split(" ")
-a=int(a)
-b=int(b)
-print(a+b)
+#baekjoon ps xx
