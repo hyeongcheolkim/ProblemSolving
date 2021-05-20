9d48493938c8cf8764abc3c72f63652657dba643
hyeongcheolkim <tori1753@naver.com>
2021-05-20 20:16:17 +0900

    ps 6064

--- JS/ps_baekjoon/6064/main.js
+++ JS/ps_baekjoon/6064/main.js
@@ -2,23 +2,27 @@ const fs = require('fs');
 //let input = fs.readFileSync('/dev/stdin').toString().split(' ');
 // let input = fs.readFileSync(`${__dirname}/stdin.txt`).toString().split(' ').map(Number);
 let input = fs.readFileSync(`${__dirname}/stdin.txt`).toString().trim().split("\n").map(e => e.split(" ").map(Number));
-console.log(input)
 let t = input[0][0];
+const gcd = function (a, b) {
+    if (b === 0)
+        return a;
+    let [maxi, mini] = [Math.max(a, b), Math.min(a, b)];
+    return gcd(mini, maxi % mini);
+}
+const lcm = function (a, b) {
+    return a * b / gcd(a, b);
+}
 for (let i = 1; i <= t; i++) {
     let [m, n, x, y] = input[i];
-    let [a, b] = [1, 1];
-    while (a !== m && b !== n) {
-        if (a === m)
-            a = 1;
-        else
-            a++;
-        if (b === n)
-            b = 1;
-        else
-            b++;
-        if (x === a && y === b) {
-            console.log(i);
-            break;
+    outer: for (; ;) {
+        for (let i = 0; i * m + x <= lcm(m, n); i++) {
+            let mod = (i * m + x) % n;
+            if (mod === y || n === y && mod === 0) {
+                console.log(i * m + x)
+                break outer;
+            }
         }
+        console.log(-1);
+        break outer;
     }
 }
