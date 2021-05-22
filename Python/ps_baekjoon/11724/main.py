93ea63b18baec901c12b05bad87a8061c1eefe6c
hyeongcheolkim <tori1753@naver.com>
2021-05-22 17:23:12 +0900

    baekjoon ps 11724

--- Python/ps_baekjoon/11724/main.py
+++ Python/ps_baekjoon/11724/main.py
@@ -1,25 +1,25 @@
 # baekjoon ps 11724
 
-import sys
 import queue
-
-n, m = map(int, input().split(" "))
+import sys
+n, m = map(int, sys.stdin.readline().split(" "))
 cnt = 0
 data = [[] for i in range(n + 1)]
 check = [False for i in range(n + 1)]
+q = queue.Queue()
 for _ in range(m):
-    a, b = map(int, input().split(" "))
+    a, b = map(int, sys.stdin.readline().split(" "))
     data[a].append(b)
     data[b].append(a)
-q = queue.Queue()
 for i in range(1,n+1):
     if not check[i]:
         q.put(i)
         cnt+=1
         check[i] = True
     while not q.empty():
-        for i in data[q.get()]:
-            if not check[i]:
-                q.put(i)
-                check[i] = True
+        fn = q.get()
+        for e in data[fn]:
+            if not check[e]:
+                q.put(e)
+                check[e] = True
 print(cnt)
