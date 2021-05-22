3ceb3b58a4ca762b9c5d0a1c89b2aed1365ec309
hyeongcheolkim <tori1753@naver.com>
2021-05-22 17:26:58 +0900

    set

--- JS/ps_baekjoon/_default/main.js
+++ JS/ps_baekjoon/_default/main.js
@@ -1,3 +1,5 @@
+//baekjoon ps xx
+
 const fs = require('fs');
 //let input = fs.readFileSync('/dev/stdin').toString().split(' ');
 let input = fs.readFileSync(`${__dirname}/stdin.txt`).toString().trim().split("\n").map(e => e.split(" ").map(Number));
