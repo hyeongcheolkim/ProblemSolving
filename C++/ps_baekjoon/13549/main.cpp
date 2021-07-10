3f3bca353aa96338b25581fb865f2927378f4eab
hyeongcheolkim <tori1753@naver.com>
2021-07-10 23:45:15 +0900

    baekjoon ps 13549 using 0-1 bfs

--- C++/ps_baekjoon/13549/main.cpp
+++ C++/ps_baekjoon/13549/main.cpp
@@ -5,7 +5,6 @@
 #include <utility>
 #include <vector>
 #include <limits>
-#include <algorithm>
 
 using namespace std;
 
@@ -14,36 +13,33 @@ int main()
     ios_base::sync_with_stdio(false);
     int n, k;
     cin >> n >> k;
-    queue<pair<int, int>> q;
+    deque<pair<int, int>> q;
     vector<int> check(200000 + 1, numeric_limits<int>::max());
-    q.push({n, 0});
+    q.push_front({n, 0});
     check[n] = 0;
     while (!q.empty())
     {
         int pos = q.front().first;
         int cnt = q.front().second;
-        q.pop();
         if (pos == k)
-        {
-            check[k] = min(check[k], cnt);
-            continue;
-        }
+            break;
+        q.pop_front();
         if (pos + 1 <= 100000 && cnt < check[pos + 1])
         {
             check[pos + 1] = cnt + 1;
-            q.push({pos + 1, cnt + 1});
+            q.push_back({pos + 1, cnt + 1});
         }
         if (pos - 1 >= 0 && cnt < check[pos - 1])
         {
             check[pos - 1] = cnt + 1;
-            q.push({pos - 1, cnt + 1});
+            q.push_back({pos - 1, cnt + 1});
         }
         if (pos * 2 <= 200000 && cnt < check[pos * 2])
         {
             check[pos * 2] = cnt;
-            q.push({pos * 2, cnt});
+            q.push_front({pos * 2, cnt});
         }
     }
-    cout << check[k];
+    cout << q.front().second;
     return 0;
 }
