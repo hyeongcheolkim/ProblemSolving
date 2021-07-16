5abd5452e5e9de820c489f599f337bfa54f8cd38
hyeongcheolkim <tori1753@naver.com>
2021-07-17 03:04:51 +0900

    baekjoon ps 1956

--- C++/ps_baekjoon/1956/main.cpp
+++ C++/ps_baekjoon/1956/main.cpp
@@ -3,13 +3,14 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <limits>
 
 using namespace std;
 
 int main()
 {
     ios_base::sync_with_stdio(false);
-    constexpr int INF = 200000000;
+    constexpr int INF = numeric_limits<int>::max() / 3;
     int v, e;
     int res = INF;
     cin >> v >> e;
@@ -18,20 +19,19 @@ int main()
         map[i][i] = 0;
     while (e--)
     {
-        static int to, go, cost;
+        int to, go, cost;
         cin >> to >> go >> cost;
         map[to][go] = cost;
     }
     for (int k = 1; k <= v; k++)
         for (int i = 1; i <= v; i++)
             for (int j = 1; j <= v; j++)
-                if (map[i][j] != INF)
-                    map[i][j] = min(map[i][j], map[i][k] + map[k][j]);
+                map[i][j] = min(map[i][j], map[i][k] + map[k][j]);
     for (int i = 1; i <= v; i++)
         for (int j = 1; j <= v; j++)
             if (i != j)
                 res = min(res, map[i][j] + map[j][i]);
-    if(res==INF)
+    if (res == INF)
         cout << -1;
     else
         cout << res;
