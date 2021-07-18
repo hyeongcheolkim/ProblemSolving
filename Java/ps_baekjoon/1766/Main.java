dfd1524560edd186b1fbc94bc1419bb9c0c59112
hyeongcheolkim <tori1753@naver.com>
2021-07-18 15:08:28 +0900

    baekjoon ps 1766 using BufferdReader

--- Java/ps_baekjoon/1766/Main.java
+++ Java/ps_baekjoon/1766/Main.java
@@ -1,15 +1,16 @@
 //baekjoon ps 1766
 
+import java.io.*;
 import java.util.*;
 import java.util.stream.*;
 
 public class Main {
     static int n, m;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder res = new StringBuilder();
-        int[] input = Stream.of(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] input = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         n = input[0];
         m = input[1];
         ArrayList<ArrayList<Integer>> map = new ArrayList<ArrayList<Integer>>() {
@@ -22,7 +23,7 @@ public class Main {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         while (m-- > 0) {
             int from, to;
-            int[] edge = Stream.of(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int[] edge = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             from = edge[0];
             to = edge[1];
             map.get(from).add(to);
@@ -41,6 +42,5 @@ public class Main {
             }
         }
         System.out.print(res);
-        sc.close();
     }
 }
