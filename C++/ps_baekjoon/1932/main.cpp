48744626d19d1a092e4ac607c5ef9d605cc6a827
hyeongcheolkim <tori1753@naver.com>
2021-07-28 01:59:07 +0900

    baekjoon ps 1932

--- C++/ps_baekjoon/1932/main.cpp
+++ C++/ps_baekjoon/1932/main.cpp
@@ -11,34 +11,15 @@ int main()
     ios_base::sync_with_stdio(false);
     int n;
     cin >> n;
-    vector<vector<int>> tree(n + 1, vector<int>(n + 1));
-    vector<vector<int>> dp(n + 1, vector<int>(n + 1));
-    for (int i = 1; i <= n * (n + 1) / 2; i++)
-    {
-        static int cnt = 0;
-        static int row = 1;
-        static int col = 1;
-        cin >> tree[row][col++];
-        cnt++;
-        if (cnt == row * (row + 1) / 2)
-        {
-            row++;
-            col = 1;
-        }
-    }
-    dp[1][1] = tree[1][1];
-    dp[2][1] = dp[1][1] + tree[2][1];
-    dp[2][2] = dp[1][1] + tree[2][2];
-    for (int row = 3; row <= n; row++)
+    vector<vector<int>> map(n + 2, vector<int>(n + 2, 0));
+    auto dp = map;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= i; j++)
+            cin >> map[i][j];
+    dp[1][1] = map[1][1];
+    for (int row = 2; row <= n; row++)
         for (int col = 1; col <= row; col++)
-        {
-            if (col == 1)
-                dp[row][col] = tree[row][col] + dp[row - 1][col];
-            else if (col == row)
-                dp[row][col] = tree[row][col] + dp[row - 1][col - 1];
-            else
-                dp[row][col] = tree[row][col] + max(dp[row - 1][col], dp[row - 1][col - 1]);
-        }
-    cout << *max_element(dp[n].begin()+1, dp[n].begin() + n + 1);
+            dp[row][col] = map[row][col] + max(dp[row - 1][col - 1], dp[row - 1][col]);
+    cout << *max_element(dp[n].begin(), dp[n].end());
     return 0;
 }
