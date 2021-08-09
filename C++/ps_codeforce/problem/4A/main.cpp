7801c015756217ececb681a9de9a806593def808
hyeongcheolkim <tori1753@naver.com>
2021-08-09 17:34:29 +0900

    codeforce ps 4A

--- C++/ps_codeforce/problem/4A/main.cpp
+++ C++/ps_codeforce/problem/4A/main.cpp
@@ -1,4 +1,4 @@
-//codeforce ps xx
+//codeforce ps 4A
 
 #include <iostream>
 
@@ -9,5 +9,11 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+    int n;
+    cin >> n;
+    if (n % 2 == 1 || n == 2)
+        cout << "NO";
+    else
+        cout << "YES";
     return 0;
 }
