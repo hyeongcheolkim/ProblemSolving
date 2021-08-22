e5153afc7bb0e54a929d8c5afbb356b83f521233
hyeongcheolkim <tori1753@naver.com>
2021-08-23 05:33:13 +0900

    set change

--- C++/ps_codeforce/contests/_default/_default/main.cpp
+++ C++/ps_codeforce/contests/_default/_default/main.cpp
@@ -1,11 +1,8 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    cin.tie(0)->sync_with_stdio(0);
     return 0;
 }
