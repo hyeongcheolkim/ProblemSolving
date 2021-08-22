f378857016c4e1be25902ce2294e3ad2194bf090
hyeongcheolkim <tori1753@naver.com>
2021-08-22 23:50:55 +0900

    baekjoon ps 2688

---
+++ C++/ps_baekjoon/2688/main.cpp
@@ -0,0 +1,35 @@
+//baekjoon ps 2688
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using ull = unsigned long long;
+ull cache[70][70];
+
+ull number_of_digits(int number, int last_value)
+{
+    ull &ret = cache[number][last_value];
+    if (ret != 0)
+        return ret;
+    for (int idx = last_value; idx <= 9; ++idx)
+        ret += number_of_digits(number - 1, idx);
+    return ret;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    for (int i = 0; i <= 9; ++i)
+        cache[1][i] = 1;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        cout << number_of_digits(n + 1, 0) << '\n';
+    }
+    return 0;
+}
