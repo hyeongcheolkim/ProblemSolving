f07bab0d88be8ea380ea07f1bf751f8444d19a23
hyeongcheolkim <tori1753@naver.com>
2021-08-27 13:59:52 +0900

    2631

--- C++/ps_baekjoon/_default/main.cpp
+++ C++/ps_baekjoon/_default/main.cpp
@@ -4,5 +4,19 @@ using namespace std;
 int main()
 {
     cin.tie(0)->sync_with_stdio(0);
+    int n;
+    cin >> n;
+    vector<int> lis;
+    lis.push_back(0);
+    for (int i = 0; i < n; ++i)
+    {
+        int num;
+        cin >> num;
+        if (lis.back() < num)
+            lis.push_back(num);
+        else
+            *lower_bound(lis.begin(), lis.end(), num) = num;
+    }
+    cout << n - (lis.size() - 1);
     return 0;
 }
