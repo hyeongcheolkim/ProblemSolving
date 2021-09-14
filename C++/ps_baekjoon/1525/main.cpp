2a0cd4fc5c8b0368b6796fe6c9a4bca5940acefb
hyeongcheolkim <tori1753@naver.com>
2021-09-14 23:01:59 +0900

    1525

---
+++ C++/ps_baekjoon/1525/main.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int SOLVED = 123456789, SIZE = 3, dr[]{0, 0, +1, -1}, dc[]{+1, -1, 0, 0};
+int puzzle[SIZE][SIZE];
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+    string str;
+    str.reserve(9);
+    for (int i = 0; i < 9; ++i)
+    {
+        int num;
+        cin >> num;
+        if (num == 0)
+            num = 9;
+        str.push_back('0' + num);
+    }
+    int init = stoi(str);
+    queue<int> q;
+    map<int, int> vst;
+    q.push(init);
+    vst[init] = 0;
+    while (!q.empty())
+    {
+        int now_int = q.front();
+        string now_str = to_string(now_int);
+        q.pop();
+        if (now_int == SOLVED)
+            break;
+        int pos = now_str.find('9');
+        int r = pos / 3;
+        int c = pos % 3;
+        for (int direction = 0; direction < 4; ++direction)
+        {
+            int nr = r + dr[direction];
+            int nc = c + dc[direction];
+            if (!(0 <= nr && nr < 3 && 0 <= nc && nc < 3))
+                continue;
+            string temp = now_str;
+            swap(temp[pos], temp[nr * 3 + nc]);
+            int next = stoi(temp);
+            if (!vst[next])
+            {
+                vst[next] = vst[now_int] + 1;
+                q.push(next);
+            }
+        }
+    }
+    cout << (vst.find(SOLVED) == vst.end() ? -1 : vst[SOLVED]);
+    return 0;
+}
