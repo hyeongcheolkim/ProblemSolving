1864ae73b231cba410f415057136c8c075406e30
hyeongcheolkim <tori1753@naver.com>
2021-09-18 03:17:05 +0900

    4179

--- C++/ps_baekjoon/4179/main.cpp
+++ C++/ps_baekjoon/4179/main.cpp
@@ -8,6 +8,24 @@ char maze[mxRC + 1][mxRC + 1];
 bool vst[mxRC + 1][mxRC + 1];
 queue<pos> qf, qj, temp;
 
+void init()
+{
+    cin >> row >> col;
+    for (int i = 1; i <= row; ++i)
+        for (int j = 1; j <= col; ++j)
+        {
+            cin >> maze[i][j];
+            if (maze[i][j] == 'F')
+                qf.emplace(i, j);
+            if (maze[i][j] == 'J')
+            {
+                maze[i][j] = '.';
+                qj.emplace(i, j);
+                vst[i][j] = true;
+            }
+        }
+}
+
 void fire_spread()
 {
     while (!qf.empty())
@@ -28,46 +46,38 @@ void fire_spread()
     qf.swap(temp);
 }
 
+void runaway()
+{
+    while (!qj.empty())
+    {
+        auto [r, c] = qj.front();
+        qj.pop();
+        for (int direction = 0; direction < 4; ++direction)
+        {
+            int nr = r + dr[direction];
+            int nc = c + dc[direction];
+            if (maze[nr][nc] == 'F' || maze[nr][nc] == '#' || vst[nr][nc])
+                continue;
+            if (maze[nr][nc] == ESCAPE)
+            {
+                cout << cnt;
+                exit(0);
+            }
+            vst[nr][nc] = true;
+            temp.emplace(nr, nc);
+        }
+    }
+    qj.swap(temp);
+}
+
 int main()
 {
     cin.tie(0)->sync_with_stdio(0);
-    cin >> row >> col;
-    for (int i = 1; i <= row; ++i)
-        for (int j = 1; j <= col; ++j)
-        {
-            cin >> maze[i][j];
-            if (maze[i][j] == 'F')
-                qf.emplace(i, j);
-            if (maze[i][j] == 'J')
-            {
-                maze[i][j] = '.';
-                qj.emplace(i, j);
-                vst[i][j] = true;
-            }
-        }
+    init();
     for (cnt = 1; !qj.empty(); ++cnt)
     {
         fire_spread();
-        while (!qj.empty())
-        {
-            auto [r, c] = qj.front();
-            qj.pop();
-            for (int direction = 0; direction < 4; ++direction)
-            {
-                int nr = r + dr[direction];
-                int nc = c + dc[direction];
-                if (maze[nr][nc] == 'F' || maze[nr][nc] == '#' || vst[nr][nc])
-                    continue;
-                if (maze[nr][nc] == ESCAPE)
-                {
-                    cout << cnt;
-                    exit(0);
-                }
-                vst[nr][nc] = true;
-                temp.emplace(nr, nc);
-            }
-        }
-        qj.swap(temp);
+        runaway();
     }
     cout << "IMPOSSIBLE";
     return 0;
