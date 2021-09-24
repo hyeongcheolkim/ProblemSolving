c70df860b8a3699856532798d73696e0a07cfd16
hyeongcheolkim <tori1753@naver.com>
2021-09-24 15:54:20 +0900

    10972

--- C++/ps_baekjoon/_default/main.cpp
+++ C++/ps_baekjoon/_default/main.cpp
@@ -1,8 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+constexpr int mxN = 1e4;
+int arr[mxN];
+
 int main()
 {
     cin.tie(0)->sync_with_stdio(0);
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+        cin >> arr[i];
+    if (next_permutation(arr, arr + n))
+        for (int i = 0; i < n; ++i)
+            cout << arr[i] << ' ';
+    else
+        cout << -1;
     return 0;
 }
