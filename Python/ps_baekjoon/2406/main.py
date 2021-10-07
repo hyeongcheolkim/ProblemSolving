f3e990a774c4113c473105079c3ff0e604495425
hyeongcheolkim <tori1753@naver.com>
2021-10-07 13:04:09 +0900

    2406

--- Python/ps_baekjoon/2406/main.py
+++ Python/ps_baekjoon/2406/main.py
@@ -23,12 +23,10 @@ def uni(a, b) -> bool:
 
 
 n, m = [int(i) for i in readline().split()]
-connect = [[int(i) for i in readline().split()] for _ in range(m)]
-graph = [0] + [[0] + [int(i) for i in readline().split()] for _ in range(n)][:n]
 uf = [i for i in range(n + 1)]
-for e in connect:
-    vertex1, vertex2 = e
-    uni(vertex1, vertex2)
+for _ in range(m):
+    uni(*[int(i) for i in readline().split()])
+graph = [0] + [[0] + [int(i) for i in readline().split()] for _ in range(n)][:n]
 for i in range(1, n + 1):
     for j in range(1, n + 1):
         if i == j or i == 1 or j == 1:
