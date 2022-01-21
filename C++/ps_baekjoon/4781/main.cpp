83854c7ce1ee7c45b75a2f5a0361fbfbed23d80a
hyeongcheolkim <tori1753@naver.com>
2022-01-21 23:14:30 +0900

    4781

---
+++ C++/ps_baekjoon/4781/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int mxM = 100;
+int n, dp[mxM * 100 + 5];
+double m;
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+    while (cin >> n >> m, !(n == 0 && m == 0.0))
+    {
+        memset(dp, 0, sizeof(dp));
+        int M = int(m * 100.0 + 0.5);
+        while (n--)
+        {
+            int c;
+            double p;
+            cin >> c >> p;
+            int P = int(p * 100.0 + 0.5);
+            for (int idx = P; idx <= M; ++idx)
+                dp[idx] = max(dp[idx], dp[idx - P] + c);
+        }
+        cout << dp[M] << '\n';
+    }
+    return 0;
+}
