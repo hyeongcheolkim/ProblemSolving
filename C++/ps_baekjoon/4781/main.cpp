10fe2d9b7a77da903f4187ded601f7aed3b6dff4
hyeongcheolkim <tori1753@naver.com>
2022-01-21 23:15:37 +0900

    4781

--- C++/ps_baekjoon/4781/main.cpp
+++ C++/ps_baekjoon/4781/main.cpp
@@ -11,13 +11,13 @@ int main()
     while (cin >> n >> m, !(n == 0 && m == 0.0))
     {
         memset(dp, 0, sizeof(dp));
-        int M = int(m * 100.0 + 0.5);
+        int M = static_cast<int>(m * 100.0 + 0.5);
         while (n--)
         {
             int c;
             double p;
             cin >> c >> p;
-            int P = int(p * 100.0 + 0.5);
+            int P = static_cast<int>(p * 100.0 + 0.5);
             for (int idx = P; idx <= M; ++idx)
                 dp[idx] = max(dp[idx], dp[idx - P] + c);
         }
