ac6936d0c56662d9734d4506a1c706484ba4f56d
hyeongcheolkim <tori1753@naver.com>
2022-01-29 23:35:25 +0900

    1158

--- C++/ps_baekjoon/1158/main.cpp
+++ C++/ps_baekjoon/1158/main.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 
 int n, k;
-deque<int> lt, res;
+deque<int> dq, res;
 
 int main()
 {
     cin.tie(0)->sync_with_stdio(0);
     cin >> n >> k;
-    lt.resize(n);
-    iota(lt.begin(), lt.end(), 1);
-    for (int i = 1; !lt.empty(); ++i)
+    dq.resize(n);
+    iota(dq.begin(), dq.end(), 1);
+    for (int i = 1; !dq.empty(); ++i)
     {
         if (i % k == 0)
-            res.push_back(lt.front());
+            res.push_back(dq.front());
         else
-            lt.emplace_back(lt.front());
-        lt.pop_front();
+            dq.emplace_back(dq.front());
+        dq.pop_front();
     }
     cout << '<';
     for_each(res.begin(), res.end() - 1, [](const auto& e) {cout << e << ", "; });
