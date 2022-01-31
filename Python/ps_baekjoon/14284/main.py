ca11b0b20e912a1e6f82d0c453ec299a4846870b
hyeongcheolkim <tori1753@naver.com>
2022-01-31 13:30:07 +0900

    14284

--- Python/ps_baekjoon/14284/main.py
+++ Python/ps_baekjoon/14284/main.py
@@ -9,11 +9,11 @@ graph = [[] for _ in range(n + 1)]
 dist = [float("inf")] * (n + 1)
 for _ in range(m):
     frm, to, cost = [int(i) for i in readline().split()]
-    graph[frm].append([cost, to])
-    graph[to].append([cost, frm])
+    graph[frm].append((cost, to))
+    graph[to].append((cost, frm))
 s, t = [int(i) for i in readline().split()]
 dist[s] = 0
-pq.put([0, s])
+pq.put((0, s))
 while not pq.empty():
     in_cost, now = pq.get()
     if dist[now] < in_cost:
@@ -21,5 +21,5 @@ while not pq.empty():
     for out_cost, next in graph[now]:
         if (next_cost := in_cost + out_cost) < dist[next]:
             dist[next] = next_cost
-            pq.put([next_cost, next])
+            pq.put((next_cost, next))
 write(f"{dist[t]}")
