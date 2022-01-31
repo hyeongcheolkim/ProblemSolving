d7a731f0e86e0f08f6ecb5507adecc425778e1fe
hyeongcheolkim <tori1753@naver.com>
2022-01-31 13:20:21 +0900

    14284

---
+++ C++/ps_baekjoon/14284/main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pos = pair<int, int>;
+
+constexpr int INF = numeric_limits<int>::max() / 2;
+int n, m, s, t;
+vector<vector<pos>> graph;
+vector<int> dist;
+priority_queue<pos, vector<pos>, greater<pos>> pq;
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> n >> m;
+    graph.resize(n + 1);
+    dist.assign(n + 1, INF);
+    while (m--)
+    {
+        int from, to, cost;
+        cin >> from >> to >> cost;
+        graph[from].emplace_back(cost, to);
+        graph[to].emplace_back(cost, from);
+    }
+    cin >> s >> t;
+    dist[s] = 0;
+    pq.emplace(0, s);
+    while (!pq.empty())
+    {
+        auto [in_cost, now] = pq.top();
+        pq.pop();
+        if (dist[now] < in_cost)
+            continue;
+        for (const auto& [out_cost, next] : graph[now])
+            if (int next_cost = in_cost + out_cost; next_cost < dist[next])
+            {
+                dist[next] = next_cost;
+                pq.emplace(next_cost, next);
+            }
+    }
+    cout << dist[t];
+    return 0;
+}
