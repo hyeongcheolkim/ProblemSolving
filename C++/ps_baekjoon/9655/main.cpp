d1f120ec722310dafdc942439022f6deea484db0
hyeongcheolkim <tori1753@naver.com>
2022-04-11 12:42:17 +0900

    9655

---
+++ C++/ps_baekjoon/9655/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+    int n;
+    cin >> n;
+    if (n % 2 == 0)
+        cout << "CY";
+    else
+        cout << "SK";
+    return 0;
+}
