f131ed5efd3aafe351118a78e2a777665d7c30bb
hyeongcheolkim <tori1753@naver.com>
2022-11-09 13:55:43 +0900

    1105

--- C++/ps_baekjoon/1105/main.cpp
+++ C++/ps_baekjoon/1105/main.cpp
@@ -1,8 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int l, r;
+
 int main()
 {
     cin.tie(0)->sync_with_stdio(0);
+    cin >> l >> r;
+    string str_l = to_string(l);
+    string str_r = to_string(r);
+    if (str_l.length() == str_r.length())
+    {
+        int mini = 0;
+        for (int i = 0; i < str_l.length(); ++i)
+        {
+            if (str_l[i] != str_r[i])
+                break;
+            if (str_l[i] == '8' && str_r[i] == '8')
+                ++mini;
+        }
+        cout << mini;
+    }
+    else
+        cout << 0;
     return 0;
 }
