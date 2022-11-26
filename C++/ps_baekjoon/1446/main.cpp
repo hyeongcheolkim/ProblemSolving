d947b5b6e3032d667eb5453be373966acc4e478f
hyeongcheolkim <tori1753@naver.com>
2022-11-27 07:36:13 +0900

    1446

---
+++ C++/ps_baekjoon/1446/main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, d, res = numeric_limits<int>::max() / 2;
+unordered_map<int, vector<pair<int, int>>> graph;
+vector<int> dist;
+
+void dfs(int now = 0, int acc = 0)
+{
+    if (now > d)
+        return;
+    if (now == d)
+    {
+        res = min(res, acc);
+        return;
+    }
+    for (const auto& [next, cost] : graph[now])
+        dfs(next, acc + cost);
+    dfs(now + 1, acc + 1);
+}
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> n >> d;
+    dist.resize(d + 1);
+    iota(dist.begin(), dist.end(), 0);
+    while (n--)
+    {
+        int from, to, cost;
+        cin >> from >> to >> cost;
+        graph[from].emplace_back(to, cost);
+    }
+    dfs();
+    cout << res;
+}
